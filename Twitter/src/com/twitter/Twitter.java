a63a38442ffea5f402364ab10c993f5baa3d5317
NikolaCarovic <nikola.carovic@yahoo.com>
2017-04-06 11:38:42 +0200

    Uveden JavaDoc za klasu Twitter

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -3,13 +3,32 @@ package com.twitter;
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
 
+/**
+ * Ova klasa predstavlja sam Twitter
+ * @author Nikola Carovic
+ * @version V1.1
+ *  
+ */
 public class Twitter {
+	
+	/**
+	 * Atribut koji predstavlja sve poruke
+	 */
 	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
 
+	/**
+	 * Metoda koja vraca poruke
+	 * @return porkue Vraca listu poruka
+	 */
 	public LinkedList<TwitterPoruka> vratiSvePoruke() {
 		return poruke;
 	}
-
+	
+	/**
+	 * Metoda koja unosi poruku
+	 * @param korisnik Ime korisnika
+	 * @param poruka Poruka koja se unosi
+	 */
 	public void unesi(String korisnik, String poruka) {
 		// Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
@@ -18,7 +37,16 @@ public class Twitter {
 		// Poruka se unosi u listu na kraj
 		poruke.addLast(tp);
 	}
-
+	
+	/**
+	 * Metoda koja vraca listu poruka sa datim tag-om
+	 * @param maxBroj Maksimum broj poruka koji moze da se vrati
+	 * @param tag Preko ovog taga se nalazi poruka
+	 * @return rezultat Vraca listu odredjenog broja poruka
+	 * @throws java.lang.RuntimeException - Tag mora da se unese
+	 * 
+	 *
+	 */
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 		if (tag == null || tag.isEmpty())
 			throw new RuntimeException("Morate uneti tag");
